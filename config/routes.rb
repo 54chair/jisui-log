7c6ed265030a33efb51935053305a9364beb453a
54chair <takukoshi3721@gmail.com>
2022-10-31 04:48:12 +0000

    createアクションとindexアクションの実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root 'pages#index'
   get 'pages/show'
-  resources :posts
+  resources :posts, only: [:index, :new, :create, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

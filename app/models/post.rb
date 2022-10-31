7799141e2c5a0d023c179ac835e00d4e0c2087c4
54chair <takukoshi3721@gmail.com>
2022-10-31 11:51:11 +0000

    default_scopeで投稿を降順で表示させる

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,5 @@ class Post < ApplicationRecord
   validates :user_id, presence: true
   validates :title, presence: true, length: { maximum: 20 }
   validates :content, presence: true, length: { maximum: 300 }
+  default_scope -> { order(created_at: :desc) }
 end

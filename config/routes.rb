6b08e77624d650bd8ee58317cf6a1e4ce046fc18
54chair <takukoshi3721@gmail.com>
2022-11-01 06:05:41 +0000

    投稿詳細ページの実装

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   get '/posts/new', to: 'posts#new'
   post '/posts', to: 'posts#create'
   get '/posts/index', to: 'posts#index'
+  get '/posts/:id', to: 'posts#show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

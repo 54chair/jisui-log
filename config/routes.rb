79eb09a090c4a3a062b6d1846f4d1190a6ab5a77
54chair <takukoshi3721@gmail.com>
2022-11-01 11:46:36 +0000

    ルーティングの修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root 'pages#index'
   get 'pages/show'
-  get '/posts/new', to: 'posts#new'
-  post '/posts', to: 'posts#create'
-  get '/posts/index', to: 'posts#index'
-  get '/posts/:id', to: 'posts#show'
+  get 'posts/new', to: 'posts#new'
+  post 'posts', to: 'posts#create'
+  get 'posts/index', to: 'posts#index'
+  get 'posts/:id', to: 'posts#show'
   post 'posts/:id/destroy', to: 'posts#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

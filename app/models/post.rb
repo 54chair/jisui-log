c1ed98b80417bf998b501ec0eb6bd54d6bd057aa
54chair <takukoshi3721@gmail.com>
2022-11-01 02:03:04 +0000

    Active Storageのバリデーションの追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,4 +5,8 @@ class Post < ApplicationRecord
   validates :title, presence: true, length: { maximum: 20 }
   validates :content, presence: true, length: { maximum: 300 }
   default_scope -> { order(created_at: :desc) }
+  validates :image,   content_type: { in: %w[image/jpeg image/gif image/png],
+                                      message: "must be a valid image format" },
+                      size:         { less_than: 5.megabytes,
+                                      message: "should be less than 5MB" }
 end

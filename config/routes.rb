c33ae31af6f45f2f9743e716e98aba3c98483de3
54chair <takukoshi3721@gmail.com>
2022-11-06 20:36:43 +0000

    Relationshipリソース用のルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
     end
   end
   resources :posts, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+  resources :relationships, only: [:create, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

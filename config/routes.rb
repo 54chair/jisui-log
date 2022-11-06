d312dcb8d43c3046b9a3e3f107411227b128c046
54chair <takukoshi3721@gmail.com>
2022-11-06 19:57:56 +0000

    followingとfollowersのルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
   root 'pages#index'
   get 'pages/show'
+  devise_for :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :posts, only: [:index, :show, :new, :create, :edit, :update, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

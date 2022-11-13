a33249eeb5d1125fda25c2cc258d69d9ec9ec7d3
54chair <takukoshi3721@gmail.com>
2022-11-13 08:17:17 +0000

    特定の投稿に対していいねしたユーザーの一覧を見れるようにする

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,4 +1,10 @@
 class LikesController < ApplicationController
+  
+  def index
+    @likes = Like.where(post_id: params[:post_id]).pluck(:user_id)
+    @likes_users = User.find(@likes)
+  end
+  
   def create
     @like = Like.new(user_id: current_user.id, post_id: params[:post_id])
     @like.save
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
     end
   end
   resources :posts, only: [:index, :show, :new, :create, :edit, :update, :destroy] do
-    resources :likes, only: [:create, :destroy]
+    resources :likes, only: [:index, :create, :destroy]
   end
   resources :relationships, only: [:create, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html

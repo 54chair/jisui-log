fea90b97757c53d351b5c12d991af635400113e6
54chair <takukoshi3721@gmail.com>
2022-11-15 01:32:55 +0000

    likesコントローラのcreateアクションとdestroyアクションに失敗したときの処理を追加

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -7,13 +7,20 @@ class LikesController < ApplicationController
   
   def create
     @like = Like.new(user_id: current_user.id, post_id: params[:post_id])
-    @like.save
-    redirect_to post_path(params[:post_id]) 
+    if @like.save
+      redirect_to post_path(params[:post_id]) 
+    else
+      flash[:danger] = "「いいね！」に失敗しました。"
+      redirect_to post_path(params[:post_id])
+    end
   end
   
   def destroy
     @like = Like.find_by(user_id: current_user.id, post_id: params[:post_id])
-    @like.destroy
-    redirect_to post_path(params[:post_id]) 
+    if @like.destroy
+      redirect_to post_path(params[:post_id])
+    else
+      flash[:danger] = "「いいね！」の削除に失敗しました。"
+    end
   end
 end

d8550dbdc3615b0469cdb71e27c0ce650962f736
54chair <takukoshi3721@gmail.com>
2022-11-23 01:57:51 +0000

    config/apprication.rbに日本語化とタイムゾーンの変更の設定を追加

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,11 @@ module JisuiLog
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    # 日本語化の設定
+    config.i18n.default_locale = :ja
+    # タイムゾーンの変更（例)created_at カラムを取り出したときに日本時間に変換されるようになる）
+    config.time_zone = 'Asia/Tokyo'
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading

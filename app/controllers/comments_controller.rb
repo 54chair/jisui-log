e8647570387d929e8cc30b5322434b0e8c5d419f
54chair <takukoshi3721@gmail.com>
2022-12-14 16:15:19 +0000

    Commentsコントローラのcreateアクションとdestroyアクションの編集

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,34 @@
 class CommentsController < ApplicationController
+  before_action :authenticate_user!
+  
   def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.new(comment_params)
+    @comment.user_id = current_user.id
+    if @comment.save
+      flash[:notice] = "コメントに成功しました。"
+      redirect_to post_path
+    else
+      flash[:alert] = "コメントの削除に失敗しました。"
+      redirect_to post_path
+    end
   end
 
   def destroy
+    @post = Post.find(params[:post_id])
+    @comment = Comment.find(params[:id])
+    if @comment.destroy
+      flash[:notice] = "コメントの削除に成功しました。"
+      redirect_to post_path
+    else
+      flash[:alert] = "コメントの削除に失敗しました。"
+      redirect_to post_path
+    end
   end
+  
+  private
+    def comment_params
+      params.require(:comment).permit(:comment)
+    end
+  
 end

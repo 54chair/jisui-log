84c4ae37b75d59b933457d59f330c5e16d47b8c6
54chair <takukoshi3721@gmail.com>
2023-03-11 03:30:27 +0000

    ユーザー名を入力していない時flashでエラーを表示させる

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
+  after_action :flash_error_if_devise_unpermitted_parameters
   
   protected
     def configure_permitted_parameters
@@ -8,6 +9,14 @@ class ApplicationController < ActionController::Base
       devise_parameter_sanitizer.permit(:account_update, keys: [:username])
       devise_parameter_sanitizer.permit(:sign_up, keys: [:avatar] )
       devise_parameter_sanitizer.permit(:account_update, keys: [:avatar] )
+    end
+    
+    def flash_error_if_devise_unpermitted_parameters
+      if devise_controller? && params[:controller] == "devise/registrations" && resource.errors.any?
+        resource.errors.each do |attr, msg|
+          flash[:error] = msg
+        end
+      end
     end
     
   private
